07915f4023dec536815462d17216c294419ddde4
Dalvin Molina <dalvingamer@gmail.com>
2021-05-04 21:38:42 -0400

    llenando formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,85 +1,105 @@
-import React, {useState} from "react";
-import styled from '@emotion/styled';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
 
 const Campo = styled.div`
-
-    display:flex;
-    margin-bottom:1rem;
-    align-items: center;
-
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
 `;
 
 const Label = styled.label`
-    flex: 0 0 100px;
+  flex: 0 0 100px;
 `;
 
 const Select = styled.select`
-    display: block;
-    border: 1px solid #e1e1e1;
-    width: 100%;
-    padding: 1rem;
-    -webkit-appearance: none;
+  display: block;
+  border: 1px solid #e1e1e1;
+  width: 100%;
+  padding: 1rem;
+  -webkit-appearance: none;
 `;
 
 const InputRadio = styled.input`
-    margin: 0 1rem;
+  margin: 0 1rem;
 `;
 
 const ButtonCotizar = styled.button`
-    background-color: #00838f;
-    font-size: 16px;
-    width: 100%;
-    padding: 1rem;
-    color: #fff;
-    text-transform: uppercase;
-    font-weight:bold;
-    border:none;
-    transition: background-color 3s ease;
-    margin-top: 1rem;
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 3s ease;
+  margin-top: 1rem;
 
-    &:hover{
-        cursor: pointer;
-        background-color: #26c6DA;
-    }
+  &:hover {
+    cursor: pointer;
+    background-color: #26c6da;
+  }
 `;
 
 const Formulario = () => {
-
   //initilization useState
   const [cotizador, setCotizador] = useState({
-        maske: '',
-        year: '',
-        plan: ''
-  })
+    marca: "",
+    year: "",
+    plan: "basico",
+  });
 
-const submitCotizador = e =>{
-  e.preventDefault;
+  const [error, setError] = useState(false);
 
-  //evaluar campo
+  //state value
+  const { marca, year, plan } = cotizador;
 
-  //porcentaje marca;
 
-  //porcentaje year;
+  const changeInfo = e =>{
 
-  //porcentaje plan;
+    setCotizador({
+      ...cotizador,
+      [e.target.name] : e.target.value
+    })
 
-}
+
+  }
+
+  const submitCotizador = e => {
+    e.preventDefault();
+
+    //evaluar campo
+
+    //porcentaje marca;
+
+    //porcentaje year;
+
+    //porcentaje plan;
+  };
 
   return (
     <form onSubmit={submitCotizador}>
       <Campo>
         <Label>Marca</Label>
-        <Select>
+        <Select 
+        name = "marca"
+        value={marca}
+        onChange = {changeInfo}
+        >
           <option value="">--Select--</option>
-          <option value="">--Americano--</option>
-          <option value="">--Europeo--</option>
-          <option value="">--Asiatico--</option>
+          <option value="americano">--Americano--</option>
+          <option value="europeo">--Europeo--</option>
+          <option value="asiatico">--Asiatico--</option>
         </Select>
       </Campo>
 
       <Campo>
         <Label>Año</Label>
-        <Select>
+        <Select 
+        name = "year"
+        value={year}
+         onChange = {changeInfo}
+        >
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -93,28 +113,28 @@ const submitCotizador = e =>{
           <option value="2012">2012</option>
         </Select>
       </Campo>
-    
-    <Campo>
+
+      <Campo>
         <Label>Plan</Label>
         <InputRadio
-            type="radio"
-            name="plan"
-            value="basico"
-        /> Basico
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange = {changeInfo}
+        />
+        Basico
         <InputRadio
-            type="radio"
-            name="plan"
-            value="completo"
-        /> Completo
-    </Campo>
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange = {changeInfo}
+        />
+        Completo
+      </Campo>
 
-    <ButtonCotizar 
-        type="button"
-    >
-        Cotizar
-    </ButtonCotizar>
-
-
+      <ButtonCotizar type="button">Cotizar</ButtonCotizar>
     </form>
   );
 };
